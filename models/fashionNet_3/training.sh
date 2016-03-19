6adc6ffbd483b9ef2b205e00062cda8e6834e74e
simpleig <tonghehehe@gmail.com>
2016-03-18 22:23:37 -0700

    debug on fashion_train_val_3.prototxt

--- models/fashionNet_3/training.sh
+++ models/fashionNet_3/training.sh
@@ -1,1 +1,3 @@
-/local2/home/tong/caffe-master/build/tools/caffe train -solver /local2/home/tong/fashionRecommendation/fashion_solver2.prototxt -weights /local2/home/tong/fashionRecommendation/data/VGG_CNN_M_2048.caffemodel
+/local2/home/tong/caffe-master/build/tools/caffe train \
+-solver /local2/home/tong/fashionRecommendation/models/fashionNet_3/fashion_solver_3.prototxt \
+-weights /local2/home/tong/fashionRecommendation/models/VGG_M_2048/VGG_CNN_M_2048.caffemodel
