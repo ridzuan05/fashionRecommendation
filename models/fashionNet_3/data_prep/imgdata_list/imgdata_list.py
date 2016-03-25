b859fb91d57e289c1319cf87f1213a0ad5ea1732
simpleig <tonghehehe@gmail.com>
2016-03-25 21:02:19 +0800

    fix bug: forget 'w' when open file

--- models/fashionNet_3/data_prep/imgdata_list/imgdata_list.py
+++ models/fashionNet_3/data_prep/imgdata_list/imgdata_list.py
@@ -1,3 +1,4 @@
+
 g_root = "/local2/home/tong/fashionRecommendation/models/fashionNet_3/data_prep/"
 
 data_type = ['train_', 'val_', 'test_']
@@ -8,7 +9,7 @@ for i in range(0,len(data_type)):
 	for j in range(0,len(attr_type)):
 		for k in range(0,len(item_type)):
 			imgData_source = open(g_root+"lmdb_list/"+data_type[i]+attr_type[j]+item_type[k])
-			imgData_target = open(g_root+"imgdata_list/"+data_type[i]+attr_type[j]+item_type[k])
+			imgData_target = open(g_root+"imgdata_list/"+data_type[i]+attr_type[j]+item_type[k],'w')
 
 			for line in imgData_source:
 				line_temp = line.split('/')
