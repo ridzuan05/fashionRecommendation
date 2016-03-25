7d4113229a2fbfb8a0b8e86ae8e698cb234bfd6d
simpleig <tonghehehe@gmail.com>
2016-03-25 19:10:54 +0800

    data resize to 244by244 & save to ssd at local/

--- data/clean_brokenImgs/cleaner/src/demo.cpp
+++ data/clean_brokenImgs/cleaner/src/demo.cpp
@@ -10,11 +10,31 @@
 using namespace cv;
 using namespace std;
 
+bool DirectoryExists( const char* pzPath )
+{
+    if ( pzPath == NULL) return false;
+
+    DIR *pDir;
+    bool bExists = false;
+
+    pDir = opendir (pzPath);
+
+    if (pDir != NULL)
+    {
+        bExists = true;    
+        (void) closedir (pDir);
+    }
+
+    return bExists;
+}
+
 int main()
 {
   ofstream myfile;
+  // myfile.open ("/home/tonghe2/fashionRecommendation/data/clean_brokenImgs/cleaner/results/broken_imgs.txt");  
   myfile.open ("/local2/home/tong/fashionRecommendation/data/clean_brokenImgs/cleaner/results/broken_imgs.txt");
   int corrupted_count=0;
+  // const char* PATH = "/home/tonghe2/fashionRecommendation/data/clean_brokenImgs/cleaner/data"; 
   const char* PATH = "/local2/home/tong/fashionRecommendation/data/Polyvore_large/1120_users/images";
   DIR *dir = opendir(PATH);
   struct dirent *entry = readdir(dir);
@@ -25,17 +45,36 @@ int main()
       std::string invalid0(entry->d_name);
       if(invalid0.compare(".")!=0 && invalid0.compare("..")!=0)
       {
+        // const char* PATH_sub_temp = "/home/tonghe2/fashionRecommendation/data/clean_brokenImgs/cleaner/data/";
         const char* PATH_sub_temp = "/local2/home/tong/fashionRecommendation/data/Polyvore_large/1120_users/images/";
+        // const char* savePath_temp = "/home/tonghe2/test/";
+        const char* savePath_temp = "/local/tong/fashionRecommendation/data/images/";
         std::string temp2(PATH_sub_temp);
+        std::string savePath_temp2(savePath_temp);
         temp2.append(entry->d_name);
+        savePath_temp2.append(entry->d_name);
+
+        if(DirectoryExists(savePath_temp2.c_str())==false)
+        {
+          const char* mkdir_temp0 = "mkdir ";
+          std::string mkdir_temp1(mkdir_temp0);
+          mkdir_temp1.append(savePath_temp);
+          mkdir_temp1.append(entry->d_name);
+          system(mkdir_temp1.c_str());
+        }
+
+        // const char* temp1 = "";
         const char* temp1 = "/items/full";
         temp2.append(temp1);
         const char* PATH_sub = temp2.c_str();
         DIR *dir_sub = opendir(PATH_sub);
         const char* temp0 = "/";
+        const char* savePath_temp3 = "/";
         temp2.append(temp0);
+        savePath_temp2.append(savePath_temp3);
         PATH_sub = temp2.c_str();
-        std::cout << PATH_sub << std::endl;
+        const char* savePath_temp4 = savePath_temp2.c_str();
+        std::cout << PATH_sub << std::endl; //each user path
         struct dirent *entry_sub = readdir(dir_sub);
         while(entry_sub != NULL)
         {
@@ -43,15 +82,40 @@ int main()
           if(invalid_sub.compare(".")!=0 && invalid_sub.compare("..")!=0)
           {
             std::string temp3(PATH_sub);
+            std::string savePath_temp5(savePath_temp4);
+
             temp3.append(entry_sub->d_name);
-            const char* PATH_img = temp3.c_str();
-            std::cout << PATH_img << std::endl;
+            savePath_temp5.append(entry_sub->d_name);
+            const char* PATH_img = temp3.c_str(); //each item path of this user
+            const char* savePath = savePath_temp5.c_str();
             cv::Mat im0 = cv::imread(PATH_img,CV_LOAD_IMAGE_COLOR);
             if(!im0.data)
             {
               corrupted_count++;
               printf("\n%s---%d\n", PATH_img, corrupted_count);
               myfile << PATH_img << "\n";
+            }
+            else
+            {
+              cv::Mat cv_img = cv::Mat(224,224,CV_8UC3,cv::Scalar(255,255,255));
+              cv::Mat temp;
+
+              //resize accoridng to the longer edge
+              if(im0.cols < im0.rows){
+                //resize
+                cv::resize(im0, temp, cv::Size(floor(im0.cols*224/im0.rows), 224));             
+                //cropping
+                temp.copyTo(cv_img(cv::Rect(floor((224-temp.cols)/2),0,temp.cols,224)));
+              }
+              else{
+                //resize
+                cv::resize(im0, temp, cv::Size(224, floor(im0.rows*224/im0.cols)));
+                //cropping
+                temp.copyTo(cv_img(cv::Rect(0,floor((224-temp.rows)/2),224,temp.rows)));
+              }
+
+              //save the new img (or replace the old img)
+              cv::imwrite(savePath,cv_img);
             }
           }
           entry_sub = readdir(dir_sub);
@@ -64,20 +128,10 @@ int main()
   closedir(dir);
   myfile.close();
 
-  #if 0
-    cv::Mat im0 = cv::imread("/home/tonghe2/f7fe11857d2852532f9f0df3e2d0b3e1f1687484.jpg", CV_LOAD_IMAGE_COLOR);
-    cv::Mat im1 = cv::imread("/home/tonghe2/ffed7c6d6906aa04a8faec4cd6b08ebb804f420f.jpg", CV_LOAD_IMAGE_COLOR);
-    if(im0.data)
-      cv::imshow("broken jpg",im0);
-    else
-      std::cout << "Broken data found!" << std::endl;
-    if(im1.data)
-      cv::imshow("broken jpg",im1);
-  #endif
-
   std::cout << std::endl;
   std::cout << "corrupted_count = " << corrupted_count << std::endl;
   std::cout << std::endl;
+
   cv::waitKey(0);
   return 0;
 }
