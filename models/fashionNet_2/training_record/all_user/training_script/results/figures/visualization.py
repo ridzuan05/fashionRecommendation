57badddf384e957989c9f2c088b8736450b2b93c
simpleig <tonghehehe@gmail.com>
2016-04-20 17:18:50 +0800

    prepare: draw recommendation .png

--- models/fashionNet_2/training_record/all_user/training_script/results/figures/visualization.py
+++ models/fashionNet_2/training_record/all_user/training_script/results/figures/visualization.py
@@ -6,6 +6,10 @@ import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 import matplotlib.pyplot as plt
 
+import os
+
+import Image
+
 # user_number
 root = '/local2/home/tong/fashionRecommendation/models/fashionNet_2/training_record/all_user/'
 all_train_size = open(root+'data_size/all_train_size.txt').readlines()
@@ -100,3 +104,150 @@ ax_left.set_title("mean_NDCG@m of [User_0, User_988]")
 plt.savefig(root+'training_script/results/figures/NDCG_at.png', bbox_inches='tight')
 plt.close('all')
 
+#=======================================================
+# drawing retrieval images
+whole_ndcg_label = []
+whole_ndcg_imgIdx = []
+whole_top_10_pos_num = []
+for u in range(0,user_num):
+	# read ndcg_mean_label_at_imgIdx.txt
+	ndcg = open(root+'training_script/results/data/t2.2.'+str(u)+'/ndcg_mean_label_at_imgIdx.txt').readlines()
+
+	# optimal idx
+	mean_ndcg_list = []
+	mean_ndcg_list.append(float(ndcg[0].strip('\r\n').split(' ')[1]))
+	for l in range(1,7):
+		mean_ndcg_list.append(float(ndcg[-4*l].strip('\r\n').split(' ')[1]))
+	optimal_idx, temp_o = max(enumerate(mean_ndcg_list), key=operator.itemgetter(1))
+	optimal_idx *= -4
+	optimal_idx += 2
+
+	# whole_ndcg_label
+	whole_ndcg_label.append(ndcg[optimal-1])
+	# whole_ndcg_imgIdx
+	whole_ndcg_imgIdx.append(ndcg[optimal+1])
+
+	# top-10 positive tuple number
+	top_10_pos_num = 0
+	for t in range(0,10):
+		if (1==int(whole_ndcg_label[-1].strip('\r\n').split(' ')[t+1])):
+			top_10_pos_num += 1
+
+	# whole_top_10_pos_num
+	whole_top_10_pos_num.append(top_10_pos_num)
+
+max_top_10_pos_num = max(whole_top_10_pos_num)
+min_top_10_pos_num = min(whole_top_10_pos_num)
+
+path_root = '/local2/home/tong/fashionRecommendation/models/fashionNet_2/data_prep/imgdata_list/val_'
+# !666!_top.txt
+
+# retrieve best top_10 outfits for 4 users
+best_root = '/local2/home/tong/fashionRecommendation/models/fashionNet_2/training_record/all_user/training_script/results/figures/charts/best/'
+best_u_count = 0
+for n in range(max_top_10_pos_num,min_top_10_pos_num-1,-1):
+	max_index = [i for i, x in enumerate(whole_top_10_pos_num) if x == n]
+	for j in range(0,len(max_index)):
+		single_max_index = max_index[j]
+		single_ndcg_label = whole_ndcg_label[single_max_index].strip('\r\n').split(' ')
+		for k in range(0,5):
+			if (0 == int(single_ndcg_label[k+1])):
+				break
+			# top-5 are all positive tuples
+			if (k == 4):
+				U_k = 'U_'+str(single_max_index)
+				if U_k not in os.listdir(best_root):
+				    os.system('mkdir '+best_root+U_k)
+				top_paths = open(path_root+str(single_max_index)+'_top.txt').readlines()
+				bot_paths = open(path_root+str(single_max_index)+'_bot.txt').readlines()
+				sho_paths = open(path_root+str(single_max_index)+'_sho.txt').readlines()
+				single_ndcg_imgIdx = whole_ndcg_imgIdx[single_max_index].strip('\r\n').split(' ')[1:10]
+				blank_image = Image.new("RGB", (224*10, 224*3))
+				for p in range(0,10):
+					temp_imgIdx = int(single_ndcg_imgIdx[p])
+
+					# read & save ./charts/best/U_k(best_u_count)/top_k(p).png
+					top_path = top_paths[temp_imgIdx].strip('\r\n').split(' ')[0]
+					# save_path = best_root+U_k+'/top_'+str(p)+'.png'
+					top_img = Image.open(top_path)
+					blank_image.paste(top_img,(p*224,224*0))
+					
+					# read & save ./charts/best/U_k(best_u_count)/bot_k(p).png
+					bot_path = bot_paths[temp_imgIdx].strip('\r\n').split(' ')[0]
+					# save_path = best_root+U_k+'/bot_'+str(p)+'.png'
+					bot_img = Image.open(bot_path)
+					blank_image.paste(bot_img,(p*224,224*1))
+
+					# read & save ./charts/best/U_k(best_u_count)/sho_k(p).png
+					sho_path = sho_paths[temp_imgIdx].strip('\r\n').split(' ')[0]
+					# save_path = best_root+U_k+'/sho_'+str(p)+'.png'
+					sho_img = Image.open(sho_path)
+					blank_image.paste(sho_img,(p*224,224*2))
+				
+				# save ./chars/best/U_k(best_u_count)/outfits_10.png
+				blank_image.save(best_root+U_k+'/outfits_10.png')
+
+				# write ./charts/best/U_k(best_u_count)/labels_10.txt
+				single_ndcg_label_fp = open(best_root+U_k+'/labels_10.txt','w')
+				single_ndcg_label_fp.write(whole_ndcg_label[single_max_index])
+				single_ndcg_label_fp.close()
+				best_u_count += 1
+
+		if (best_u_count == 4):
+			n = min_top_10_pos_num
+			break
+
+# retrieve worst top_10 outfits for 4 users
+worst_root = '/local2/home/tong/fashionRecommendation/models/fashionNet_2/training_record/all_user/training_script/results/figures/charts/worst/'
+worst_u_count = 0
+for n in range(min_top_10_pos_num,max_top_10_pos_num+1):
+	min_index = [i for i, x in enumerate(whole_top_10_pos_num) if x == n]
+	for j in range(0,len(min_index)):
+		single_min_index = min_index[j]
+		single_ndcg_label = whole_ndcg_label[single_min_index].strip('\r\n').split(' ')
+		for k in range(0,5):
+			if (0 == int(single_ndcg_label[k+1])):
+				break
+			# top-5 are all positive tuples
+			if (k == 4):
+				U_k = 'U_'+str(single_min_index)
+				if U_k not in os.listdir(worst_root):
+				    os.system('mkdir '+worst_root+U_k)
+				top_paths = open(path_root+str(single_min_index)+'_top.txt').readlines()
+				bot_paths = open(path_root+str(single_min_index)+'_bot.txt').readlines()
+				sho_paths = open(path_root+str(single_min_index)+'_sho.txt').readlines()
+				single_ndcg_imgIdx = whole_ndcg_imgIdx[single_min_index].strip('\r\n').split(' ')[1:10]
+				blank_image = Image.new("RGB", (224*10, 224*3))
+				for p in range(0,10):
+					temp_imgIdx = int(single_ndcg_imgIdx[p])
+
+					# read & save ./charts/best/U_k(best_u_count)/top_k(p).png
+					top_path = top_paths[temp_imgIdx].strip('\r\n').split(' ')[0]
+					# save_path = best_root+U_k+'/top_'+str(p)+'.png'
+					top_img = Image.open(top_path)
+					blank_image.paste(top_img,(p*224,224*0))
+					
+					# read & save ./charts/best/U_k(best_u_count)/bot_k(p).png
+					bot_path = bot_paths[temp_imgIdx].strip('\r\n').split(' ')[0]
+					# save_path = best_root+U_k+'/bot_'+str(p)+'.png'
+					bot_img = Image.open(bot_path)
+					blank_image.paste(bot_img,(p*224,224*1))
+
+					# read & save ./charts/best/U_k(best_u_count)/sho_k(p).png
+					sho_path = sho_paths[temp_imgIdx].strip('\r\n').split(' ')[0]
+					# save_path = best_root+U_k+'/sho_'+str(p)+'.png'
+					sho_img = Image.open(sho_path)
+					blank_image.paste(sho_img,(p*224,224*2))
+				
+				# save ./chars/best/U_k(best_u_count)/outfits_10.png
+				blank_image.save(worst_root+U_k+'/outfits_10.png')
+
+				# write ./charts/best/U_k(best_u_count)/labels_10.txt
+				single_ndcg_label_fp = open(worst_root+U_k+'/labels_10.txt','w')
+				single_ndcg_label_fp.write(whole_ndcg_label[single_max_index])
+				single_ndcg_label_fp.close()
+				worst_u_count += 1
+
+		if (worst_u_count == 4):
+			n = max_top_10_pos_num
+			break
