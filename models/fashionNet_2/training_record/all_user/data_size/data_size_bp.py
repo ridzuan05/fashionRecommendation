218fe41411950a51dfa79edf002a95d00540b3b2
simpleig <tonghehehe@gmail.com>
2016-04-20 11:13:53 +0800

    4*prepare: new test_iter

--- models/fashionNet_2/training_record/all_user/data_size/data_size_bp.py
+++ models/fashionNet_2/training_record/all_user/data_size/data_size_bp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-data_type = ['train', 'val', 'test']
+data_type = ['train_', 'val_', 'test_']
 
 l_root = "/local2/home/tong/fashionRecommendation/models/fashionNet_2/data_prep/"
 g_root = '/local2/home/tong/fashionRecommendation/models/fashionNet_2/data_prep/general_list/'
@@ -10,7 +10,7 @@ for_length = open(g_root+'tuples_test_posi.txt').readlines()
 user_num = 1+int(for_length[-1].strip('\r\n').split(' ')[0])
 
 for i in range(0,len(data_type)):
-	all_user_tupleSize = open(size_root+'data_size/all_'+data_type[i]+'_size.txt','w') # open all_train/val/test_size.txt
+	all_user_tupleSize = open(size_root+'data_size/all_'+data_type[i]+'size.txt','w') # open all_train/val/test_size.txt
 	for u in range(0,user_num):
 		temp_data = open(l_root+"imgdata_list/"+data_type[i]+str(u)+'_top.txt').readlines()
 		temp_size = len(temp_data)
