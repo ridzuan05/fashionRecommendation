5d60828415f329f751ec6c1ca1ba58dfbde1ca47
simpleig <tonghehehe@gmail.com>
2016-04-21 14:43:03 +0800

    6*prepare: fashionNet_2 test

---
+++ models/fashionNet_2/training_record/all_user_test/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for u in $( seq 0 988 )
+do 
+	/local2/home/tong/fashionRecommendation/models/fashionNet_2/training_record/all_user_test/training_script/control_${u}.py 2>&1 | tee -a run.log
+done
