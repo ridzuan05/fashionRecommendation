8e2cdfb1ac9d8b936a536ee1fbcd644ddcd084c3
simpleig <tonghehehe@gmail.com>
2016-04-25 17:01:33 +0800

    2*prepare: t8.1_finetune_train_val/train_val_prototxt

--- models/fashionNet_8/training_record/t8.1_finetune_train_val/train_val_prototxt/fashion_train_val_8_k.py
+++ models/fashionNet_8/training_record/t8.1_finetune_train_val/train_val_prototxt/fashion_train_val_8_k.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+
 # source fashion_train_val_8.prototxt for modification
 s_root = '/local2/home/tong/fashionRecommendation/models/fashionNet_8/training_record/t8.1_finetune_train_val/train_val_prototxt/'
 
@@ -9,10 +11,13 @@ s_fp = open(s_root+'fashion_train_val_8.prototxt').readlines()
 # user_number
 user_num = 800
 
+if 'fashion_train_val_8_k' not in os.listdir(s_root):
+    os.system('mkdir '+s_root+'fashion_train_val_8_k')
+
 # generate fashion_train_val_8_k.prototxt for each user
 for u in range(0,user_num):
 	# open k_fp
-	k_fp = open(s_root+'fashion_train_val_8_'+str(u)+'.prototxt','w')
+	k_fp = open(s_root+'fashion_train_val_8_k/fashion_train_val_8_'+str(u)+'.prototxt','w')
 	# write fashion_train_val_8_k.prototxt
 	for i in range(0,len(s_fp)):
 	        temp = s_fp[i].split('^')
