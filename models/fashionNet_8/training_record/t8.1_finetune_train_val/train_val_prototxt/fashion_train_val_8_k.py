1626dbf63407ac962ac4dcd41509d5ac6cda5c0a
simpleig <tonghehehe@gmail.com>
2016-04-25 16:53:03 +0800

    prepare: t8.1_finetune_train_val/train_val_prototxt

---
+++ models/fashionNet_8/training_record/t8.1_finetune_train_val/train_val_prototxt/fashion_train_val_8_k.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# source fashion_train_val_8.prototxt for modification
+s_root = '/local2/home/tong/fashionRecommendation/models/fashionNet_8/training_record/t8.1_finetune_train_val/train_val_prototxt/'
+
+# open source fashion_train_val_8.prototxt
+s_fp = open(s_root+'fashion_train_val_8.prototxt').readlines()
+
+# user_number
+user_num = 800
+
+# generate fashion_train_val_8_k.prototxt for each user
+for u in range(0,user_num):
+	# open k_fp
+	k_fp = open(s_root+'fashion_train_val_8_'+str(u)+'.prototxt','w')
+	# write fashion_train_val_8_k.prototxt
+	for i in range(0,len(s_fp)):
+	        temp = s_fp[i].split('^')
+	        if(len(temp)==3):
+	            temp[1] = str(u)
+	            temp = temp[0]+temp[1]+temp[2]
+	        else:
+	            temp = temp[0]
+	        k_fp.write(temp)
+	k_fp.close()
