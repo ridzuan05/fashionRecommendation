a59c34152855ef74f3989661564d7e9334018a54
simpleig <tonghehehe@gmail.com>
2016-04-29 15:00:03 +0800

    prep: Net_3 general training

--- models/fashionNet_3/testing.sh
+++
@@ -1,5 +0,0 @@
-/local2/home/tong/caffe-master/build/tools/caffe test \
--model /local2/home/tong/fashionRecommendation/fashion_train_val_3.prototxt \
--weights /local2/home/tong/fashionRecommendation/data/record/fashionNet3_rank/fashion_predict3_rank.caffemodel \
--gpu 0 \
--iterations 800
--- models/fashionNet_3/training.sh
+++
@@ -1,3 +0,0 @@
-/local2/home/tong/caffe-master/build/tools/caffe train \
--solver /local2/home/tong/fashionRecommendation/models/fashionNet_3/fashion_solver_3.prototxt \
--weights /local2/home/tong/fashionRecommendation/models/VGG_M_2048/VGG_CNN_M_2048.caffemodel
