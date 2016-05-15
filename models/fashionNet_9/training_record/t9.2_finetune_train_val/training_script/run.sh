bb1f2f0da364a5e0f96fe9bcf322301516e2c8ba
simpleig <tonghehehe@gmail.com>
2016-05-15 12:43:21 +0800

    resume Net9 new finetune train

--- models/fashionNet_9/training_record/t9.2_finetune_train_val/training_script/run.sh
+++ models/fashionNet_9/training_record/t9.2_finetune_train_val/training_script/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for u in $( seq 0 799 )
+for u in $( seq 324 799 )
 do 
 	/home/dell/fashionRecommendation/models/fashionNet_9/training_record/t9.2_finetune_train_val/training_script/control_k/control_${u}.py 2>&1 | tee -a run.log
 done
